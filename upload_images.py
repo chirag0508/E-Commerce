7dbee2f533c0a9a96d1e5fb47479fca069d38c8c
chirag0508 <chirag.workdesk05@gmail.com>
2026-03-08 13:43:11 +0530

    Cloudinary media setup fix

--- upload_images.py
+++ upload_images.py
@@ -4,16 +4,19 @@ import os
 
 cloudinary.config(
     cloud_name="dfnvqgyn1",
-    api_key="YOUR_API_KEY",
-    api_secret="YOUR_API_SECRET"
+    api_key="945634964155354",
+    api_secret="5E4F_0Y_X9OiS6qKSNGiNQB4Jf4"
 )
 
 folder = "media/products"
 
 for file in os.listdir(folder):
     path = os.path.join(folder, file)
-    if os.path.isfile(path):
+
+    if os.path.isfile(path) and os.path.getsize(path) > 0:
         print("Uploading:", file)
         cloudinary.uploader.upload(path, folder="products")
+    else:
+        print("Skipping empty file:", file)
 
 print("Upload complete")
