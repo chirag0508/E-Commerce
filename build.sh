19d70742619c66d9c3ce1e00b866c7e9169e3ae6
chirag0508 <chirag.workdesk05@gmail.com>
2026-03-08 11:29:57 +0530

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 build.sh
---------------------------------
+
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
---------------------------------
