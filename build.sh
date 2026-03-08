833cced78f9ac250f574fa3298a3bf00f582bcfa
chirag0508 <chirag.workdesk05@gmail.com>
2026-03-08 11:15:36 +0530

    updated build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+build.sh
+--------------------------------
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
+--------------------------------
