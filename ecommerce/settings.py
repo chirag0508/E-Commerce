00be6e2eab2a7d6467ca5879e1952756bd9030e5
chirag0508 <chirag.workdesk05@gmail.com>
2026-05-10 13:03:49 +0530

    Fix SMTP port 465

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,8 +132,9 @@ AUTH_USER_MODEL = 'accounts.User'
 # Set EMAIL_HOST_USER and EMAIL_HOST_PASSWORD as environment variables on Render.
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp-relay.brevo.com')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
